8c05ded5534746586847f23b2e6a075e4846f08a
Thalita Mariano <8.litah.8@gmail.com>
2021-07-05 13:00:20 -0300

    setting global css - <defaunt html font REM>, -<style disable>, -<global variables>, -<changes in cursor>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import styled from 'styled-components'
+import {GlobalStyle} from './styles/global';
 
-const Title = styled.h1`
-color:#8257e6;
-font-size:64px;
-`
 export function App() {
   return (
   <div className="App">
-    <Title>
-     <h1 className="title" >Ní Hâo </h1>
-    </Title>
+     <h1 className="title" > Ní Hâo </h1>
+    <GlobalStyle/>
   </div>
   );
 }
