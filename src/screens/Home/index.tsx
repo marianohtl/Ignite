74525e168920585d4d5ab1718f92f6df6a40bdcd
Thalita Mariano <tmariano@g.globo>
2024-02-11 23:42:05 -0300

    adding to start fooApp, initializing fundamentals with react native

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import {Text} from 'react-native';
+import {styles} from './styles'
+
+export default function Home()
+{
+  return(
+    <>
+      <Text style={styles.textColor}> baba </Text>
+      <Text> is </Text>
+      <Text> red </Text>
+    </>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default const styles = StyleSheet.create({
+    textColor:{
+      color: '#f12',
+      padding: 10,
+      fontSize: 30
+    }
+  });
